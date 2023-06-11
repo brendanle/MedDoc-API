8a59c2ddbc852e41541c0d6fbc36e5ab89a5b0e6
Brendan Le <brendanle1228@gmail.com>
2023-06-11 00:09:59 -0400

    added CORS configuration to allow PATCH

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
